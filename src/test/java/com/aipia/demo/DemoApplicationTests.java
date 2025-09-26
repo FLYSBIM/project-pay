2436a63c930c3e09cf04990819e1a367a39f67b5
FLYSBIM <jj2358971@gmail.com>
2025-09-27 00:42:12 +0900

    Spring Boot 프로젝트 생성

---
+++ src/test/java/com/aipia/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aipia.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
