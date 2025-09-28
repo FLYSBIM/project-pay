a26b9b904b64c527caaf18effc7e9ac203490c00
FLYSBIM <jj2358971@gmail.com>
2025-09-29 00:47:44 +0900

    feat: MemberController 추가

---
+++ src/main/java/com/aipia/demo/member/presentation/MemberController.java
@@ -0,0 +1,35 @@
+package com.aipia.demo.member.presentation;
+
+import com.aipia.demo.member.application.MemberService;
+import com.aipia.demo.member.dto.MemberCreateRequestDto;
+import com.aipia.demo.member.dto.MemberResponseDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/members")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @PostMapping
+    public ResponseEntity<MemberResponseDto> signUp(@RequestBody MemberCreateRequestDto requestDto) {
+        MemberResponseDto responseDto = memberService.createMember(requestDto);
+        URI location = URI.create("/api/members/" + responseDto.getId());
+        return ResponseEntity.created(location).body(responseDto);
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<MemberResponseDto> getMember(@PathVariable String email) {
+        MemberResponseDto responseDto = memberService.findByEmail(email);
+        return ResponseEntity.ok(responseDto);
+    }
+}
---
+++ src/main/java/com/aipia/demo/member/presentation/MemberControllerAdvice.java
@@ -0,0 +1,22 @@
+package com.aipia.demo.member.presentation;
+
+import com.aipia.demo.member.exception.EmailAlreadyExistsException;
+import com.aipia.demo.member.exception.MemberNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class MemberControllerAdvice {
+
+    @ExceptionHandler(MemberNotFoundException.class)
+    public ResponseEntity<String> handleMemberNotFoundException(MemberNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<String> handleEmailAlreadyExistsException(EmailAlreadyExistsException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
+}
